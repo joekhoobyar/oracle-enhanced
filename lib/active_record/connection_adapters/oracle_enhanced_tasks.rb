607c88bac6ca6c824280a4f006245808ddfbb156
Joe Khoobyar <joe@ankhcraft.com>
2010-05-19 18:45:23 -0400

    Revert "bugfix: rake crashes with oracle_enhanced" NOTE: this was due to a Rails 3 bug that is now fixed in edge
    This reverts commit 9b227ed313243a097f8aa242c1b08a10ec11c4ca.

--- lib/active_record/connection_adapters/oracle_enhanced_tasks.rb
+++ lib/active_record/connection_adapters/oracle_enhanced_tasks.rb
@@ -1,13 +1,10 @@
 # implementation idea taken from JDBC adapter
-if defined?(Rake.application) and Rake.application and
-  (ActiveRecord::Base.configurations[Rails.env].nil? or
-      ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'oracle_enhanced')
-then
+if defined?(Rake.application) && Rake.application && ActiveRecord::Base.configurations[RAILS_ENV]['adapter'] == 'oracle_enhanced'
   oracle_enhanced_rakefile = File.dirname(__FILE__) + "/oracle_enhanced.rake"
   if Rake.application.lookup("environment")
     # rails tasks already defined; load the override tasks now
     load oracle_enhanced_rakefile
-  elsif ActiveRecord::Base.configurations[Rails.env]
+  else
     # rails tasks not loaded yet; load as an import
     Rake.application.add_import(oracle_enhanced_rakefile)
   end
